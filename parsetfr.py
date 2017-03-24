e883b666a786a9008502a7d323eaf726fc4fb135
Vargnatt <kudosblade@gmail.com>
2017-03-24 18:43:14 +0800

    csv

--- parsetfr.py
+++ parsetfr.py
@@ -1,7 +1,7 @@
 
 # pylint: disable = W0312, E0401, C0103, W0611
 
-import json
+import csv
 import os
 import tensorflow as tf
 
@@ -16,11 +16,20 @@ for files in os.listdir(path):
         # filelist.append(path+files)
         for example in tf.python_io.tf_record_iterator(path + files):
             result = tf.train.Example.FromString(example)
-            featurelist.append(result)
-dict_name = "records.txt"
+            dic = {}
+            dic["video_id"] = result.features.feature["video_id"].value.byte_list.value[0]
+            lst = []
+            for i in range(len(result.features.feature["video_id"].value.int64_list.value)):
+                lst.append(
+                    int(result.features.feature["video_id"].value.int64_list.value[i]))
+            dic["labels"] = lst
+            featurelist.append(dic)
+dict_name = "records.csv"
 
-thefile = open(dict_name, 'w')
+# thefile = open(dict_name, 'wb')
 # with open(dict_name, 'w') as outfile:
 #     json.dump(featurelist, outfile)
-for item in featurelist:
-    thefile.write("%s\n" % item)
+# f=open(dict_name,'w')
+with open(dict_name, 'w') as output:
+    writer = csv.writer(output)
+    writer.writerows(featurelist)
