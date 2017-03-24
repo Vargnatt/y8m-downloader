4cad596862d5c0ef4ce7ea7353687cebd8d6099d
Vargnatt <kudosblade@gmail.com>
2017-03-24 18:58:52 +0800

    fix writing

--- parseTFR.py
+++ parseTFR.py
@@ -1,7 +1,7 @@
 
 # pylint: disable = W0312, E0401, C0103, W0611
 
-import csv
+import json
 import os
 import tensorflow as tf
 
@@ -17,19 +17,20 @@ for files in os.listdir(path):
         for example in tf.python_io.tf_record_iterator(path + files):
             result = tf.train.Example.FromString(example)
             dic = {}
-            dic["video_id"] = result.features.feature["video_id"].byte_list.value[0]
+            dic["video_id"] = result.features.feature["video_id"].bytes_list.value[0]
             lst = []
-            for i in range(len(result.features.feature["video_id"].int64_list.value)):
+            for i in range(len(result.features.feature["labels"].int64_list.value)):
                 lst.append(
-                    int(result.features.feature["video_id"].int64_list.value[i]))
+                    int(result.features.feature["labels"].int64_list.value[i]))
             dic["labels"] = lst
             featurelist.append(dic)
-dict_name = "records.csv"
+dict_name = "records.json"
 
 # thefile = open(dict_name, 'wb')
 # with open(dict_name, 'w') as outfile:
 #     json.dump(featurelist, outfile)
 # f=open(dict_name,'w')
+# print(type(featurelist))
+# print(len(featurelist))
 with open(dict_name, 'w') as output:
-    writer = csv.writer(output)
-    writer.writerows(featurelist)
+    json.dump(featurelist,output)
