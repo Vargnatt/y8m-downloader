383f15730fb588cd4a48717a1f4cdb2677c2386b
Vargnatt <kudosblade@gmail.com>
2017-04-03 15:59:35 +0800

    add

--- prepro.py
+++ prepro.py
@@ -1,7 +1,7 @@
 # pylint: disable = W0312, E0401, C0103, W0611
 import os
 import cv2
-
+import multiprocessing
 
 vid_path = ''
 dst_path = ''
@@ -17,7 +17,7 @@ for files in os.listdir(vid_path):
         success, image = vidcap.read()
         if count % 10 == 0:
             name = '{0:0>5}'.format(seq)
-            cv2.imwrite(vid_dst+'/'+name, image)
+            cv2.imwrite(vid_dst + '/' + name+'.png', image)
             seq += 1
         count += 1
 
@@ -30,3 +30,31 @@ for files in os.listdir(vid_path):
 #     print('Read a new frame: ', success)
 #     cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
 #     count += 1
+
+## Multi Thread
+# filelist = os.listdir(vid_path)
+# locker = [0] * len(filelist)
+
+
+# def process(files):
+#     vid_dst = dst_path + files
+#     os.mkdir(vid_dst)
+#     vidcap = cv2.VideoCapture(vid_path + files)
+#     count = 0
+#     success = True
+#     seq = 0
+#     while success:
+#         success, image = vidcap.read()
+#         if count % 10 == 0:
+#             name = '{0:0>5}'.format(seq)
+#             cv2.imwrite(vid_dst + '/' + name + '.png', image)
+#             seq += 1
+#         count += 1
+
+
+# p = multiprocessing.Pool()
+# for f in filelist:
+#     p.apply_async(process, [f])
+
+# p.close()
+# p.join()
