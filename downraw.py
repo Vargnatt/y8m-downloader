79738a4824819b2ed8c2d7eef3b5e92070feb392
Vargnatt <kudosblade@gmail.com>
2017-04-12 10:35:16 +0800

    a

--- downraw.py
+++ downraw.py
@@ -89,9 +89,6 @@ with open('{}/ckpt', 'w') as ckf:
         for e in lists[idx:-1]:
             # read and down
             with youtube_dl.YoutubeDL(dl_opts) as dl:
-                dl.download([e])
-            dl = {}
-            with youtube_dl.YoutubeDL(ydl_opts) as dl:
                 dl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
             check_e += 1
             json.dump(dic, ckf)
