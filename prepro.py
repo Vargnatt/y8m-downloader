3cb97a742170f0504b24323353c11e4530e1ae82
Vargnatt <kudosblade@gmail.com>
2017-05-01 12:32:38 +0800

    a

--- prepro.py
+++ prepro.py
@@ -1,23 +1,38 @@
 # pylint: disable = W0312, E0401, C0103, W0611
 import os
 import cv2
+import argparse
 import multiprocessing
 
-vid_path = ''
-dst_path = ''
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dst', help='folder to save extracted frames')
+parser.add_argument('--src', help='folder contains videos')
+# parser.add_argument('--lib',help='opencv or scikit-video')
+
+args = parser.parse_args()
+dst_path = args.dst
+vid_path = args.src
+
+if not vid_path.endswith('/'):
+    vid_path = vid_path + '/'
+if not dst_path.endswith('/'):
+    dst_path = dst_path + '/'
 
 for files in os.listdir(vid_path):
     vid_dst = dst_path + files
     os.mkdir(vid_dst)
     vidcap = cv2.VideoCapture(vid_path + files)
+    fps=vidcap.get()
+    stride=int(fps/3)
     count = 0
     success = True
     seq = 0
-    while success:
+    while vidcap.isOpened():
         success, image = vidcap.read()
-        if count % 10 == 0:
+        if count % stride == 0:
             name = '{0:0>5}'.format(seq)
-            cv2.imwrite(vid_dst + '/' + name+'.png', image)
+            cv2.imwrite(vid_dst + '/' + name + '.png', image)
             seq += 1
         count += 1
 
@@ -31,7 +46,7 @@ for files in os.listdir(vid_path):
 #     cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
 #     count += 1
 
-## Multi Thread
+# Multi Thread
 # filelist = os.listdir(vid_path)
 # locker = [0] * len(filelist)
 
